3a424882ed84de932844be3857970ff7406db58d
Muharrem <muharrem.kursun25@gmail.com>
2019-07-13 13:15:43 +0300

    travis deneme

--- src/main/java/lombok/App.java
+++ src/main/java/lombok/App.java
@@ -12,7 +12,7 @@ public class App
         Person1 person1 = new Person1("suayb","simsek",1);
         Person2  person2 = new Person2("emir", "aglamaz",20);
 
-        System.out.println("person1 : "+person1.getAd()+"  "+ person1.getSoyad()+"  "+person1.getYas());
-        System.out.println("person2 : "+person2.getAd()+"  "+ person2.getSoyad()+"  "+person2.getYas());
+        System.out.println("person1 : "+person1.getAd()+"  "+ person1.getSoyad()+"   "+person1.getYas());
+        System.out.println("person2 : "+person2.getAd()+"  "+ person2.getSoyad()+"   "+person2.getYas());
     }
 }
